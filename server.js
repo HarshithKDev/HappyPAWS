fd17fca6473986ccc97d36f8155a905cb0872cdc
Harshith K <harshithkotian999@gmail.com>
2025-07-21 14:20:22 +0530

    Initial commit: HappyPAWS

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const db = require('./config/db');
+const authRoutes = require('./routes/auth');
+const session = require('express-session');
+
+app.use(express.static('public'));
+
+// Configure session management
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set to true if using HTTPS
+}));
+
+// Use the auth routes
+app.use('/auth', authRoutes);
+
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
